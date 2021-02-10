13f1886a2cf3afb50c5baca3434f24309f9e47f3
Susan Wright <susan.wright@unity3d.com>
2021-02-10 10:44:47 -0700

    Tower only 'sees' creeps layer

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -6,15 +6,20 @@ public class Tower : MonoBehaviour
 {
 
     public float range;
-    
+    private Collider[] m_Targets;
+    private Collider m_Collider;
+    private LayerMask m_Mask;
+
     void Start()
     {
-        
+        m_Collider = GetComponent<Collider>();
+        m_Mask = LayerMask.GetMask("Ground Creeps");
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    void FixedUpdate()
     {
-        
+        var bounds = m_Collider.bounds;
+        m_Targets = Physics.OverlapCapsule(bounds.max, bounds.min, range, m_Mask);
+        Debug.Log(m_Targets.Length);
     }
 }

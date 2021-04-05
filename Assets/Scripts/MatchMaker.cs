be96ec43b697fdd2b88ea2d6c2d666317c996974
Susan Wright <susan.wright@unity3d.com>
2021-04-05 17:03:18 -0600

    Add matchmaker

---
+++ Assets/Scripts/MatchMaker.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class MatchMaker : MonoBehaviour
+{
+    public string username;
+
+    private Text m_searchingText;
+    private bool m_searching;
+
+    [Serializable]
+    private class MatchData
+    {
+        public string id;
+        public string server;
+        public string attacker_id;
+        public string attacker_username;
+        public string defender_id;
+        public string defender_username;
+    }
+
+    public void Start()
+    {
+        Text[] texts = GetComponentsInChildren<Text>(true);
+        
+        m_searchingText = texts.Single(val => val.name == "Searching");
+        m_searching = false;
+    }
+    
+    public void SetUsername(string input)
+    {
+        username = input.ToLower();
+    }
+
+    public void FindMatch()
+    {
+        UnityWebRequest webReq = new UnityWebRequest(string.Format("localhost:3000/search/{0}", username));
+        webReq.method = "PUT";
+        webReq.SendWebRequest();
+        m_searching = true;
+
+        StartCoroutine(CheckMatchMaker());
+    }
+
+    public void FixedUpdate()
+    {
+        if (!m_searching) return;
+        
+        int elipses = (int) Mathf.Repeat(Time.fixedTime, 4);
+        m_searchingText.text = "Searching" + new String('.', elipses);
+    }
+
+    private IEnumerator CheckMatchMaker()
+    {
+        UnityWebRequest webReq = new UnityWebRequest(string.Format("localhost:3000/search/{0}", username));
+        webReq.downloadHandler = new DownloadHandlerBuffer();
+        yield return webReq.SendWebRequest();
+
+        if (webReq.result == UnityWebRequest.Result.Success)
+        {
+            MatchData data = JsonUtility.FromJson<MatchData>(webReq.downloadHandler.text);
+            m_searching = false;
+            m_searchingText.text = string.Format("Match Found!\n{0}\nvs.\n{1}", data.attacker_username, data.defender_username);
+        }
+
+        yield return new WaitForSeconds(5);
+        
+        StartCoroutine(CheckMatchMaker());
+    }
+}
+
